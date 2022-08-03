d7f1ada17c27fec441db9bf905e9bba7dc067d1c
elnurra <elnurra@code.edu.az>
2022-08-03 15:26:10 +0400

    Task-2 successfully completed

---
+++ TaskForMinInArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TaskForMinInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min,index=0;
+            int[] arr = { 23,56,56,47,87,17,97,89};
+            min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (min > arr[i]) 
+                {
+                    min = arr[i];
+                    index = i;
+                }
+            }
+            Console.WriteLine("Minimum value in arr: " + min + " In index: " + index);
+
+        }
+    }
+}
---
+++ TaskForSearching/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TaskForSearching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "My name is Gustavo but you can call me Gus";
+            bool condition = false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i]=='a')
+                {
+                    condition = true;
+                }
+            }
+            if (condition) 
+            {
+                Console.WriteLine("The symbol 'a' exists in the given context");
+            }
+            else
+            {
+                Console.WriteLine("The symbol 'a' unfortunately does not exist in this context");
+            }
+            
+        }
+    }
+}
